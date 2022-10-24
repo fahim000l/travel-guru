15dee6121fd376df9406563313a49f97dce2c089
Md Fahim Faisal <mdfahimfaisal000@gmail.com>
2022-10-24 21:46:12 +0600

    finally done

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env_apiKey,
-    authDomain: process.env_authDomain,
-    projectId: process.env_projectId,
-    storageBucket: process.env_storageBucket,
-    messagingSenderId: process.env_messagingSenderId,
-    appId: process.env_appId
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
